3a07f97b4b793b8db850a4db7668345e6d135a79
Yuri-RC <cttyuriresende@gmail.com>
2022-09-29 13:20:04 -0300

    fix: altera jwt helper

--- app/backend/src/helpers/jwt.ts
+++ app/backend/src/helpers/jwt.ts
@@ -1,15 +1,15 @@
-import * as jwt from 'jsonwebtoken';
+import { verify, sign } from 'jsonwebtoken';
 import jwtDTO from '../dtos/jwtDTO';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'jwt_secret';
 
 const create = (payload: jwtDTO): string => {
-  const token = jwt.sign(payload, JWT_SECRET);
+  const token = sign(payload, JWT_SECRET);
   return token;
 };
 
 const validate = (token: string): jwtDTO => {
-  const decoded = jwt.verify(token, JWT_SECRET);
+  const decoded = verify(token, JWT_SECRET);
   return decoded as jwtDTO;
 };
 
