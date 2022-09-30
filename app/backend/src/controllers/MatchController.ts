f2bf5902f9b156a65f0f3875089052486ed3ff2f
Yuri-RC <cttyuriresende@gmail.com>
2022-09-30 18:13:09 -0300

    feat: valida se id existe na base de dados

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -15,11 +15,18 @@ class TeamController {
     const token = req.headers.authorization;
     try {
       jwt.validate(token as string);
-      const _match = req.body;
-      const { status, match, message } = await this.service.create({ ..._match, inProgress: true });
-      return res.status(status).json(match || { message });
     } catch {
       return res.status(401).json({ message: 'Token must be a valid token' });
+    }
+    try {
+      const _match = req.body;
+      const { status, match, message } = await this.service.create(
+        { ..._match, inProgress: true },
+      );
+      return res.status(status).json(match || { message });
+    } catch {
+      const message = 'There is no team with such id!';
+      return res.status(404).json({ message });
     }
   };
 
